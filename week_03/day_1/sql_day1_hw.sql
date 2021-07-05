60726c7ad46c9185d4fed83219229bdf5f01c699
Anne Braae <annebraae@gmail.com>
2021-07-05 20:06:56 +0100

    Update sql_day1_hw.sql

--- week_03/day_1/sql_day1_hw.sql
+++ week_03/day_1/sql_day1_hw.sql
@@ -32,6 +32,8 @@ COUNT(*) AS employees_port_spain
 FROM employees 
 WHERE country = 'Portugal' OR country = 'Spain';
 
+/* MVP */
+/* Q5 */
 /*Count the number of pay_details records lacking a local_account_no.*/
 
 SELECT 
@@ -39,6 +41,8 @@ COUNT (*) AS pay_details_no_acc
 FROM pay_details 
 WHERE local_account_no IS NULL;
 
+/* MVP */
+/* Q6 */
 /*Are there any pay_details records lacking both a local_account_no and iban number?*/
 
 SELECT 
@@ -47,7 +51,7 @@ FROM pay_details
 WHERE local_account_no IS NULL
 AND iban IS NULL;
 
-/*No. */
+/*Ans: No. */
 
 /*MVP*/
 /*Q7*/
@@ -57,6 +61,8 @@ SELECT first_name, last_name
 FROM employees 
 ORDER BY last_name ASC NULLS LAST;
 
+/* MVP */
+/* Q8 */
 /*Get a table of employees first_name, last_name and country, ordered alphabetically first by country and then by last_name (put any NULLs last).*/
 
 SELECT first_name, last_name, country
@@ -64,6 +70,8 @@ FROM employees
 ORDER BY country ASC NULLS LAST,
 last_name ASC NULLS LAST;
 
+/* MVP */
+/* Q9 */
 /*Find the details of the top ten highest paid employees in the corporation.*/
 
 SELECT *
@@ -71,7 +79,8 @@ FROM employees
 ORDER BY salary DESC NULLS LAST 
 LIMIT 10;
 
-
+/* MVP */
+/* Q10 */
 /*Find the first_name, last_name and salary of the lowest paid employee in Hungary.*/
 
 SELECT *
@@ -80,14 +89,16 @@ WHERE country = 'Hungary'
 ORDER BY salary ASC NULLS LAST 
 LIMIT 1;
 
+/* MVP */
+/* Q11 */
 /*How many employees have a first_name beginning with ‘F’?*/
-/*Q11*/
 
 SELECT
 COUNT(*) AS employees_F
 FROM employees 
 WHERE first_name LIKE 'F%';
 
+/* MVP */
 /*Q12*/
 /*Find all the details of any employees with a ‘yahoo’ email address?*/
 
@@ -95,6 +106,7 @@ SELECT *
 FROM employees 
 WHERE email LIKE '%yahoo%';
 
+/* MVP */
 /*Q13*/
 /*Count the number of pension enrolled employees not based in either France or Germany.*/
 
@@ -104,6 +116,7 @@ FROM employees
 WHERE country != 'France' OR country != 'Germany'
 AND pension_enrol = TRUE;
 
+/* MVP */
 /*Q14*/
 /*What is the maximum salary among those employees in the ‘Engineering’ department who work 1.0 full-time equivalent hours (fte_hours)?*/
 
@@ -113,6 +126,7 @@ FROM employees
 WHERE department = 'Engineering'
 AND fte_hours = 1;
 
+/* MVP */
 /*Q15*/
 /*Return a table containing each employees first_name, last_name, full-time equivalent hours (fte_hours), salary, 
  * and a new column effective_yearly_salary which should contain fte_hours multiplied by salary.*/
@@ -126,7 +140,8 @@ CONCAT(fte_hours*salary) AS effective_yearly_salary
 FROM employees 
 WHERE first_name IS NOT NULL and last_name IS NOT NULL;
 
-/*Question 16.
+/* Extension */
+/*Q16
 The corporation wants to make name badges for a forthcoming conference. 
 Return a column badge_label showing employees’ first_name and last_name joined together with their department in the following style: 
 ‘Bob Smith - Legal’. Restrict output to only those employees with stored first_name, last_name and department.
@@ -139,33 +154,49 @@ WHERE first_name IS NOT NULL
 AND last_name IS NOT NULL 
 AND department IS NOT NULL;
 
+/* Extension */
 /*
-Question 17.
+Q17
 One of the conference organisers thinks it would be nice to add the year of the employees’ start_date to the badge_label to celebrate long-standing colleagues, 
-in the following style ‘Bob Smith - Legal (joined 1998)’. Further restrict output to only those employees with a stored start_date.
-
-[If you’re really keen - try adding the month as a string: ‘Bob Smith - Legal (joined July 1998)’]
-
-Hints*/
-
-SELECT EXTRACT(YEAR FROM start_date) AS start_year
-FROM employees
+in the following style ‘Bob Smith - Legal (joined 1998)’. Further restrict output to only those employees with a stored start_date.*/
 
 SELECT 
-CONCAT(first_name, EXTRACT(YEAR FROM start_date))
-FROM employees;
-
-
-SELECT 
-CONCAT(first_name,' ', last_name, ' - ', department, ' joined (', EXTRACT(YEAR FROM start_date), ')') AS badge_label
+CONCAT(first_name,' ', last_name, ' - ', department, ' (joined ', EXTRACT(YEAR FROM start_date), ')') AS badge_label
 FROM employees 
 WHERE first_name IS NOT NULL
 AND last_name IS NOT NULL 
 AND department IS NOT NULL;
 
+/*[If you’re really keen - try adding the month as a string: ‘Bob Smith - Legal (joined July 1998)’]*/
+/*I tried, but the below 'works' but the spacing is weird.*/
+SELECT  
+CONCAT(first_name,' ', last_name, ' - ', department, ' (joined ', 
+	TO_CHAR(start_date, 'Month'),
+	EXTRACT(YEAR FROM start_date), ')') 
+	AS badge_label
+FROM employees 
+WHERE first_name IS NOT NULL
+AND last_name IS NOT NULL 
+AND department IS NOT NULL;
 
-
+/* Extension */
 /*
-Question 18.
-Return the first_name, last_name and salary of all employees together with a new column called salary_class with a value 'low' where salary is less than 40,000 and value 'high' where salary is greater than or equal to 40,000.
- */*/
+Q 18.
+Return the first_name, last_name and salary of all employees together with a new column called salary_class with a value 'low' where salary 
+is less than 40,000 and value 'high' where salary is greater than or equal to 40,000.
+ */
+ 
+SELECT  
+first_name,
+last_name,
+salary,
+CASE 
+	WHEN salary < 40000 THEN 'low'
+	WHEN salary >= 40000 THEN 'high'
+END AS salary_class
+FROM employees 
+WHERE first_name IS NOT NULL
+AND last_name IS NOT NULL 
+AND salary IS NOT NULL;
+
+
