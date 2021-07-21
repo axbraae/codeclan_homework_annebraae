e707b92fd8a53a2f46e0d302c269546eaf1cd810
Anne Braae <annebraae@gmail.com>
2021-07-21 19:33:55 +0100

    Update app.R

--- week_05/day_3_shiny_advanced_ui/app.R
+++ week_05/day_3_shiny_advanced_ui/app.R
@@ -4,15 +4,16 @@ library(shinythemes)
 library(tidyverse)
 library(CodeClanData)
 
+all_teams <- unique(olympics_overall_medals$team)
 
 # ui ----------------------------------------------------------------------
 
 ui <- fluidPage(
     theme = shinytheme("superhero"),
     
-    titlePanel(tags$h1("Five Country Medal Comparison")),
+    titlePanel(tags$h1("Olympics Medals Summary")),
     
-    navbarPage("Olympics",
+    navbarPage("",
                tabPanel("Five Country Medal Comparison",
                         fluidRow(
                  column(6, radioButtons(inputId = "season_input",
@@ -25,21 +26,34 @@ ui <- fluidPage(
                               choices = c("Gold", "Silver", "Bronze")
                               )
                         ),
-                 column(12, plotOutput("medal_plot")
+                 column(12, plotOutput("medal_5_plot")
                         )                
             )
         ),
-        tabPanel("The Olympic Website",
-            tags$a("Click here", href = "https://www.olympics.com/")
+        tabPanel("All Medal Winners Comparison",
+                 fluidRow(
+                 column(6, selectInput("team_input",
+                             "Which team?",
+                             choices = all_teams)
+                        ),
+                 #column(6, radioButtons(inputId = "season_input", #removed for now
+                              #"Summer or Winter Olympics",
+                              #choices = c("Summer", "Winter"))
+                        #),
+                 column(12, plotOutput("medal_plot")
+                        )
+                 )
         ),
-        tabPanel("All Medal Winners Comparison")
+        tabPanel("The Olympic Website",
+                 tags$a("Click here", href = "https://www.olympics.com/")
+        )
     )
 )
 
 
 # server ------------------------------------------------------------------
 server <- function(input, output) {
-    output$medal_plot <- renderPlot({
+    output$medal_5_plot <- renderPlot({
         olympics_overall_medals %>%
             filter(team %in% c("United States",
                                "Soviet Union",
@@ -60,6 +74,22 @@ server <- function(input, output) {
             theme(
                 legend.position = "none"
             )
+    })
+    
+    output$medal_plot <- renderPlot({
+        olympics_overall_medals %>% 
+            filter(team == input$team_input) %>% 
+            #filter(season == input$season_input) %>% 
+            ggplot() +
+            aes(x = medal, y = count, fill = medal) +
+            geom_col() +
+            facet_wrap(~season) +
+            scale_fill_manual(
+                values = c(
+                    "Gold" = "gold",
+                    "Silver" = "gray75",
+                    "Bronze" = "orange"
+                ))
     })
 }
 
